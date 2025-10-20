b4d2b61a10553181b56639badaff4a477a07a454
Ahmed <ahmedelahmmar@gmail.com>
2025-10-20 14:31:19 +0300

    repo restructuring

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,10 @@
 #include <bits/stdc++.h>
 #include <iostream>
 
-#include "demos/exceptions/exceptions.hpp"
-#include "demos/oop/diamond_inheritance/diamond_inheritance.hpp"
-
 int main(int argc, char **argv)
 {
     static_cast<void>(argc);
     static_cast<void>(argv);
-
-    Demos::DiamondInheritance::Run();
 
     return 0;
 }
